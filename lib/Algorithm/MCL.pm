3c510038910f9895605418fdfe2e999818afe3d2
Pinkhas Nisanov <pinkhasn@cpan.org>
2012-03-03 13:03:11 +0000

    Import of PINKHASN/Algorithm-MCL-0.002 from CPAN.
    gitpan-cpan-distribution: Algorithm-MCL
    gitpan-cpan-version:      0.002
    gitpan-cpan-path:         PINKHASN/Algorithm-MCL-0.002.tar.gz
    gitpan-cpan-author:       PINKHASN
    gitpan-cpan-maturity:     released

--- lib/Algorithm/MCL.pm
+++ lib/Algorithm/MCL.pm
@@ -1,88 +1,7 @@
 package Algorithm::MCL;
 
-# ABSTRACT: perl module implementing Markov Cluster Algorithm using PDL 
+# ABSTRACT: perl module implementing Markov Cluster Algorithm using PDL
 
-=head1 SYNOPSIS
-
-use Algorithm::MCL;
-
-my $obj1 = new MyClass;
-my $ref2 = {};
-my $ref3 = \"abc";
-my $ref4 = \$val1;
-my $ref5 = [];
-
-my $mcl1 = Algorithm::MCL->new();
-
-# create graph by adding edges
-$mcl1->addEdge($obj1, $ref2);
-$mcl1->addEdge($obj1, $ref3);
-$mcl1->addEdge($ref2, $ref3);
-$mcl1->addEdge($ref3, $ref4);
-$mcl1->addEdge($ref4, $ref5);
-
-# run MCL algorithm on created graph
-my $clusters1 = $mcl1->run();
-
-# get clusters
-foreach my $cluster ( @$clusters1 ) {
-   print "Cluster size: ". scalar @$cluster. "\n";
-}
-
-
-####################################
-
-my $val1 = \"aaa";
-my $val2 = \"bbb";
-my $val3 = \"ccc";
-my $val4 = \"ddd";
-my $val5 = \"eee";
-
-my $mcl2 = Algorithm::MCL->new();
-$mcl2->addEdge($val1, $val2);
-$mcl2->addEdge($val1, $val3);
-$mcl2->addEdge($val2, $val3);
-$mcl2->addEdge($val3, $val4);
-$mcl2->addEdge($val4, $val5);
-
-my $clusters2 = $mcl2->run();
-
-# get clusters
-foreach my $cluster ( @$clusters2 ) {
-    print "Found Cluster\n";
-    foreach my $vertex ( @$cluster ) {
-        print "  Cluster element: ". $$vertex. "\n";
-    }
-}
-
-
-
-=head1 DESCRIPTION
-
-This module is perl implementation of Markov Cluster Algorithm (MCL) based on Perl Data Language (PDL).
-
-MCL is algorithm of finding clusters of vertices in graph. More information about MCL can be found at L<http://micans.org/mcl/>. There is also perl script implementing MCL - minimcl L<http://www.micans.org/mcl/scripts/minimcl>.
-
-This module try to solve two problems:
-- easy integration MCL in perl scripts and modules. Algorithm::MCL accept references as input and every reference will be found later in some cluster.
-- performance and scale. Algorithm::MCL use Perl Data Language for most of its processing. That means Algorithm::MCL can run very fast on very big clusters. Main Algorithm::MCL procedures are written with "pdlpp".
-
-
-=head1 METHODS
-
-=method new()
-
-create new Algorithm::MCL object that accumulate graph edges and process data.
-
-=method addEdge($ref1, $ref2, $distance)
-
-add new edge to graph. first two parameters are reference to vertex objects. third parameter is "distance measurement" between vetices. "distance measurement" should be number between 0 and 1. if "distance measurement" is not defined it set to 1.
-
-=method run()
-
-apply MCL algorithm on graph. return reference to array that every element is reference to cluser array.
-
-=cut
 
 
 use PDL;
@@ -248,6 +167,118 @@ sub getIdxObj {
 
 __PACKAGE__->meta->make_immutable();
 
+
+
+
+
+=pod
+
+=head1 NAME
+
+Algorithm::MCL - perl module implementing Markov Cluster Algorithm using PDL
+
+=head1 VERSION
+
+version 0.002
+
+=head1 SYNOPSIS
+
+    use Algorithm::MCL;
+    
+    my $obj1 = new MyClass;
+    my $ref2 = {};
+    my $ref3 = \"abc";
+    my $ref4 = \$val1;
+    my $ref5 = [];
+    
+    my $mcl1 = Algorithm::MCL->new();
+    
+    # create graph by adding edges
+    $mcl1->addEdge($obj1, $ref2);
+    $mcl1->addEdge($obj1, $ref3);
+    $mcl1->addEdge($ref2, $ref3);
+    $mcl1->addEdge($ref3, $ref4);
+    $mcl1->addEdge($ref4, $ref5);
+
+    # run MCL algorithm on created graph
+    my $clusters1 = $mcl1->run();
+    
+    # get clusters
+    foreach my $cluster ( @$clusters1 ) {
+       print "Cluster size: ". scalar @$cluster. "\n";
+    }
+    
+    
+    ####################################
+    
+    my $val1 = \"aaa";
+    my $val2 = \"bbb";
+    my $val3 = \"ccc";
+    my $val4 = \"ddd";
+    my $val5 = \"eee";
+    
+    my $mcl2 = Algorithm::MCL->new();
+    $mcl2->addEdge($val1, $val2);
+    $mcl2->addEdge($val1, $val3);
+    $mcl2->addEdge($val2, $val3);
+    $mcl2->addEdge($val3, $val4);
+    $mcl2->addEdge($val4, $val5);
+    
+    my $clusters2 = $mcl2->run();
+    
+    foreach my $cluster ( @$clusters2 ) {
+        print "Found Cluster\n";
+        foreach my $vertex ( @$cluster ) {
+            print "  Cluster element: $$vertex \n";
+        }
+    }
+
+=head1 DESCRIPTION
+
+This module is perl implementation of Markov Cluster Algorithm (MCL) based on Perl Data Language (PDL).
+
+MCL is algorithm of finding clusters of vertices in graph. More information about MCL can be found at L<http://micans.org/mcl/>. There is also perl script implementing MCL - minimcl L<http://www.micans.org/mcl/scripts/minimcl>.
+
+This module try to solve two problems:
+
+=over 2
+
+=item *
+
+easy integration MCL in perl scripts and modules. Algorithm::MCL accept references as input and every reference will be found later in some cluster.
+
+=item *
+
+performance and scale. Algorithm::MCL use Perl Data Language for most of its processing and should run very fast on very big clusters. Main Algorithm::MCL procedures are written with "pdlpp".
+
+=back
+
+=head1 METHODS
+
+=head2 new()
+
+create new Algorithm::MCL object that accumulate graph edges and process data.
+
+=head2 addEdge($ref1, $ref2, $distance)
+
+add new edge to graph. first two parameters are reference to vertex objects. third parameter is "distance measurement" between vetices. "distance measurement" should be number between 0 and 1. if "distance measurement" is not defined it set to 1.
+
+=head2 run()
+
+apply MCL algorithm on graph. return reference to array that every element is reference to cluser array.
+
+=head1 AUTHOR
+
+Pinkhas Nisanov <pinkhas@nisanov.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Pinkhas Nisanov.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
 
 
 __DATA__
